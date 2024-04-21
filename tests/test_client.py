72a614268fc9e119195f45f831460824fcf8aac7
Anna “CyberTailor” <cyber@sysrq.in>
2024-04-21 21:59:02 +0500

    tests: add test for raw API call

--- tests/test_client.py
+++ tests/test_client.py
@@ -13,14 +13,22 @@ from repology_client.types import Package
 
 @pytest.mark.vcr
 @pytest.mark.asyncio(scope="session")
-async def test_get_packages_empty(session: aiohttp.ClientSession) -> None:
+async def test_raw_api(session: aiohttp.ClientSession):
+    problems = await repology_client.api("/repository/freebsd/problems",
+                                         session=session)
+    assert len(problems) != 0
+
+
+@pytest.mark.vcr
+@pytest.mark.asyncio(scope="session")
+async def test_get_packages_empty(session: aiohttp.ClientSession):
     with pytest.raises(InvalidInput):
         await repology_client.get_packages("", session=session)
 
 
 @pytest.mark.vcr
 @pytest.mark.asyncio(scope="session")
-async def test_get_packages_notfound(session: aiohttp.ClientSession) -> None:
+async def test_get_packages_notfound(session: aiohttp.ClientSession):
     with pytest.raises(EmptyResponse):
         project = uuid.uuid5(uuid.NAMESPACE_DNS, "repology.org").hex
         await repology_client.get_packages(project, session=session)
@@ -28,7 +36,7 @@ async def test_get_packages_notfound(session: aiohttp.ClientSession) -> None:
 
 @pytest.mark.vcr
 @pytest.mark.asyncio(scope="session")
-async def test_get_packages(session: aiohttp.ClientSession) -> None:
+async def test_get_packages(session: aiohttp.ClientSession):
     packages = await repology_client.get_packages("firefox", session=session)
 
     firefox_pkg: Package | None = None
@@ -50,28 +58,28 @@ async def test_get_packages(session: aiohttp.ClientSession) -> None:
 
 @pytest.mark.vcr
 @pytest.mark.asyncio(scope="session")
-async def test_get_projects_simple(session: aiohttp.ClientSession) -> None:
+async def test_get_projects_simple(session: aiohttp.ClientSession):
     projects = await repology_client.get_projects(count=200, session=session)
     assert len(projects) == 200
 
 
 @pytest.mark.vcr
 @pytest.mark.asyncio(scope="session")
-async def test_get_400_projects(session: aiohttp.ClientSession) -> None:
+async def test_get_400_projects(session: aiohttp.ClientSession):
     projects = await repology_client.get_projects(count=400, session=session)
     assert len(projects) > 200
 
 
 @pytest.mark.vcr
 @pytest.mark.asyncio(scope="session")
-async def test_get_projects_start_and_end(session: aiohttp.ClientSession) -> None:
+async def test_get_projects_start_and_end(session: aiohttp.ClientSession):
     with pytest.warns(UserWarning):
         await repology_client.get_projects("a", "b", session=session)
 
 
 @pytest.mark.vcr
 @pytest.mark.asyncio(scope="session")
-async def test_get_projects_search_failed(session: aiohttp.ClientSession) -> None:
+async def test_get_projects_search_failed(session: aiohttp.ClientSession):
     with pytest.raises(EmptyResponse):
         project = uuid.uuid5(uuid.NAMESPACE_DNS, "repology.org").hex
         await repology_client.get_projects(search=project, session=session)
@@ -79,6 +87,6 @@ async def test_get_projects_search_failed(session: aiohttp.ClientSession) -> Non
 
 @pytest.mark.vcr
 @pytest.mark.asyncio(scope="session")
-async def test_get_projects_search(session: aiohttp.ClientSession) -> None:
+async def test_get_projects_search(session: aiohttp.ClientSession):
     projects = await repology_client.get_projects(search="firefox", session=session)
     assert "firefox" in projects
